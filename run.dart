0175221999f8a3608ff6e454d473feb9433fca80
Samia <samia@gmail.com>
2025-12-16 10:31:55 +0300

    flutter statup

--- run.dart
+++ run.dart
@@ -1,3 +1,27 @@
-String demoFunction() {
-  return "This is a demo function.";
-}
+// // // String demoFunction() {
+// // //   return "This is a demo function.";
+// // // }
+
+
+// // void main() {
+// //   print("Hello, World!");
+// // }
+
+// // void main() {
+// //   print(add(2, 3));
+// // }
+
+// // int add(int a, int b) {
+// //   return a + b;
+// // }
+
+// // void main(){
+// //   int a = 3;
+// //   a = a + 4;
+// //   print(a);
+// // }
+
+
+// void main() {
+//   print("Hello $name");
+// }
